462d81ad39bc62d2e43c97f932e84022e3043253
lavanyadirisala <lavanyabhimavaram@gmail.com>
2025-10-26 23:21:41 +0530

    Modified list

--- src/main/java/com/ai/SpringRAG/service/RetrievalService.java
+++ src/main/java/com/ai/SpringRAG/service/RetrievalService.java
@@ -8,9 +8,9 @@ import java.util.List;
 public class RetrievalService {
     //in-memory list (can modify it to file or db)
     private final List<String> knowledgeBase = List.of(
-            "Java is a popular programming language.",
-            "Spring Boot helps to build microservices quickly.",
-            "RAG stands for Retrieval-Augmented Generation."
+            "Java is a popular programming language. Java is a high-level, object-oriented, and multi-platform programming language used to build a wide variety of applications, from smartphone apps and enterprise software to cloud services and big data engines",
+            "Spring Boot is an open-source Java framework used for programming standalone, production-grade Spring-based applications with a bundle of libraries that make project startup and management easier.",
+            "RAG stands for Retrieval-Augmented Generation, a technique that improves AI responses by having large language models (LLMs) retrieve external information before generating an answer. It can also refer to a Red, Amber, Green status system for project management, or a general term for ragged clothing or pieces of cloth"
     );
 
     public String retrieveContext(String query) {
